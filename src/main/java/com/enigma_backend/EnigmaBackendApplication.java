27bdcb19fc00211cd7a4076f66d0340e04cc6f9b
Mauro Raibida <mauroraibida@gmail.com>
2024-08-05 20:56:35 -0300

    feat: projeto base em Spring Boot

---
+++ src/main/java/com/enigma_backend/EnigmaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.enigma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/enigma_backend/EnigmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
